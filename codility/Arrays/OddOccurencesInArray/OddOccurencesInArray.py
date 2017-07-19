b6225ce0236caf6210a378110066fe16c073143b
sirbega <sirbega@gmail.com>
2017-07-19 22:17:49 +0200

    still fixing

--- codility/Arrays/OddOccurencesInArray/OddOccurencesInArray.py
+++ codility/Arrays/OddOccurencesInArray/OddOccurencesInArray.py
@@ -2,24 +2,18 @@
 """Show the odd occurence in an array. Codility problem."""
 
 
-def Solution(A):
+def solution(A):
     """Return the odd occurence in an array."""
-    B = []
-    for i in range(0, len(A)):
-        B.append(False)
-    for j in range(0, len(A)):
-        if B[j] is False:
-            for k in range(j+1, len(A)):
-                if B[k] is False:
-                    if A[j] == A[k]:
-                        B[j] = True
-                        B[k] = True
-                        break
-    for l in range(0, len(A)):
-        if B[l] is False:
+    A = sorted(A)
+    duz = len(A) - 1
+    i = 0
+    while i < duz:
+        if A[i] == A[i + 1]:
+            i += 2
+        else:
             break
-    return A[l]
+    return A[i]
 
 
 niz = [9, 3, 9, 3, 9, 7, 9]
-print(Solution(niz))
+print(solution(niz))
