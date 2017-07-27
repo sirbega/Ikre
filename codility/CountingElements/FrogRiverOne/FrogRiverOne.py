61cabdf06f3dcd183d9d4217dbb229ee76a08263
sirbega <sirbega@gmail.com>
2017-07-27 17:41:28 +0200

    FrogRiverOne elegant

--- codility/CountingElements/FrogRiverOne/FrogRiverOne.py
+++ codility/CountingElements/FrogRiverOne/FrogRiverOne.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""Solutioin to the FrogRiverOne problem from Codility."""
+"""Solution to the FrogRiverOne problem from Codility."""
 
 
 def solution(X, A):
     B = [False] * X
-    duz = len(A) + 1
-    chk = -1
+    duz = len(A)
     count = X
     for i in range(0, duz):
-        if count > 0:
-            if not B[A[i] - 1]:
-                B[A[i] - 1] = True
-                count -= 1
-        else:
-            chk = i - 1
-            break
-    return chk
+        if not B[A[i] - 1]:
+            B[A[i] - 1] = True
+            count -= 1
+            if count == 0:
+                return i
+    return -1
 
 
 unos = [1, 3, 1, 4, 3, 5, 4, 2]
