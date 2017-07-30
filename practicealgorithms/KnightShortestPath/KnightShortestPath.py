6ae0f610d112531797f891474fad35e5d4b02fee
sirbega <sirbega@gmail.com>
2017-07-30 22:29:40 +0200

    trying KnightShortPath

---
+++ practicealgorithms/KnightShortestPath/KnightShortestPath.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Finds the shortest path for the chess figure knight."""
+
+
+def solution(A, B):
+    chk = True
+    matrix = [[-1 for x in range(A + 5)] for y in range(B + 5)]
+    matrix[2][2] = 0
+    i = 0
+    while chk:
+        chk = False
+        for xi in range(2, A + 2):
+            for yi in range(2, B + 2):
+                if matrix[xi][yi] == i:
+                    if matrix[xi + 2][yi + 1] == -1:
+                        matrix[xi + 2][yi + 1] = i + 1
+                        chk = True
+                    if matrix[xi + 2][yi - 1] == -1:
+                        matrix[xi + 2][yi - 1] = i + 1
+                        chk = True
+                    if matrix[xi - 2][yi + 1] == -1:
+                        matrix[xi - 2][yi + 1] = i + 1
+                        chk = True
+                    if matrix[xi - 2][yi - 1] == -1:
+                        matrix[xi - 2][yi - 1] = i + 1
+                        chk = True
+                    if matrix[xi + 1][yi + 2] == -1:
+                        matrix[xi + 1][yi + 2] = i + 1
+                        chk = True
+                    if matrix[xi + 1][yi - 2] == -1:
+                        matrix[xi + 1][yi - 2] = i + 1
+                        chk = True
+                    if matrix[xi - 1][yi + 2] == -1:
+                        matrix[xi - 1][yi + 2] = i + 1
+                        chk = True
+                    if matrix[xi - 1][yi - 2] == -1:
+                        matrix[xi - 1][yi - 2] = i + 1
+                        chk = True
+        i += 1
+        if i == 100000000:
+            return -2
+        if not chk:
+            return -1
+        if not matrix[A + 2][B + 2] == -1:
+            return i
+
+
+print(solution(99, 99))
---
+++ practicealgorithms/KnightShortestPath/readme.txt
@@ -0,0 +1,3 @@
+Start coordinates and finish coordinates are given.
+Find the shortest path a knight (chess figure) needs
+to move from start to finish
