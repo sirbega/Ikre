25107fdbe6a91d1e958eaa3079bc1cc9d9b7c8cd
sirbega <sirbega@gmail.com>
2019-01-19 17:20:49 +0100

    Make it public

---
+++ neumbot/bot.js
@@ -0,0 +1,26 @@
+var Twit = require('twit');
+var TwitterBot = require('node-twitterbot').TwitterBot;
+
+var Bot = new TwitterBot({
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function dateDiffInDays(a, b) {
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const a = new Date("2018-10-07"),
+    b = new Date(),
+    difference = dateDiffInDays(a, b);
+
+var phrase = difference + ". dan nakon izbora, Neum još uvijek nije Monte Carlo."
+
+Bot.tweet(phrase);
